b7fcc809b80c5d12cb2c5a15668e65e740275e43
Gabriel <gabrielcoding97@gmail.com>
2019-10-08 00:21:21 +0000

    added form and styled comments section

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,12 +3,3 @@ class Comment < ApplicationRecord
   belongs_to :place
 end
 
-  RATINGS = {
-    'one skull': '1_skull',
-    'two skulls': '2_skulls',
-    'three skulls': '3_skulls',
-    'four skulls': '4_skulls',
-    'five skulls': '5_skulls'
-  }
-end
-
