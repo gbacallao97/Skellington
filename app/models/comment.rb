350c927f4e0250b4ab5eb79991f0db0aca266e54
Gabriel <gabrielcoding97@gmail.com>
2019-10-15 00:27:45 +0000

    ratings

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'one skull': '1_skull',
+    'two skulls': '2_skulls',
+    'three skulls': '3_skulls',
+    'four skulls': '4_skulls',
+    'five skulls': '5_skulls'
+  }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
-
-
