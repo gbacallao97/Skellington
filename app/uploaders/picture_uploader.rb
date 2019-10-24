3db26da74ba5030e8deec7036ba2388cf8c10598
Gabriel <gabrielcoding97@gmail.com>
2019-10-24 03:19:48 +0000

    fixed places and comments

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,6 +13,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  class Photo < ActiveRecord::Base
+    mount_uploader :picture, PictureUploader
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
--- db/schema.rb
+++ db/schema.rb
@@ -28,10 +28,13 @@ ActiveRecord::Schema.define(version: 2019_10_21_030653) do
 
   create_table "photos", force: :cascade do |t|
     t.text "caption"
+    t.integer "place_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -52,11 +55,6 @@ ActiveRecord::Schema.define(version: 2019_10_21_030653) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
